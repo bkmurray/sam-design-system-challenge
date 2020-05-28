3fcf6c350def2fd93014af91729eeb18cd6d00cd
Matthew Finnell <38668285+matthewcfinnell@users.noreply.github.com>
2020-05-28 17:02:35 -0400

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
+      path: '',
+      redirectTo: '/workspace/downloads', patchMatch: 'full'
+  },
+  {
     path: 'workspace/downloads',
     loadChildren: () => import('./components/sds-download/sds-download.module').then(m => m.SdsDownloadModule)
+  },
+  {
+      path: '**',
+      redirectTo: '/workspace/downloads', patchMatch: 'full'
   }
 ];
 
