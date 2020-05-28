01950a73b7670c19e1304308e6a71de8c446624a
Christy Hermansen <christy.hermansen@gsa.gov>
2020-05-28 04:59:56 -0400

    Removed a bad merge.  Replaced footer with latest example from sam-styles.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  {
-  	path: '',
-  	redirectTo: '/system-account/editor', pathMatch: 'full'
-  },
-  {
-    path: 'system-account/editor',
-    loadChildren: () => import('./system-account/system-account-editor/system-account-editor.module').then(m => m.SystemAccountEditorModule)
-  },
-  {
-  	path: '**',
-  	redirectTo: '/system-account/editor', pathMatch: 'full'
-  }
 ];
 
 @NgModule({
