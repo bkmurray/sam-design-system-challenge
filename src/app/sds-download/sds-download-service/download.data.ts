a84f782904dbee1289f969c877a7a86338a44f79
matthewcfinnell <38668285+matthewcfinnell@users.noreply.github.com>
2020-05-28 10:17:53 -0400

    developed download service

---
+++ src/app/sds-download/sds-download-service/download.data.ts
@@ -0,0 +1,227 @@
+export let downloadData = [
+	{
+  "totalRecords": 50,
+  "mydownloads": [
+    {
+      "id": 749566,
+      "userId": "bhargav.boppana@gsa.gov",
+      "domain": "Contract Data",
+      "format": "Full CSV",
+      "fileName": "ContractData-20200429-022140",
+      "searchType": "Search Results",
+      "createDate": "May 13, 2020",
+      "expireDate": "May 20, 2020",
+      "status": "Expired"
+    },
+    {
+      "id": 749159,
+      "userId": "bhargav.boppana@gsa.gov",
+      "domain": "Entity Information",
+      "format": "Full CSV",
+      "fileName": "EntityInformation-20200504-085129_Exclusion",
+      "searchType": "Search Results",
+      "createDate": "May 4, 2020",
+      "expireDate": "May 11, 2020",
+      "status": "Expired"
+    },
+    {
+      "id": 749158,
+      "userId": "bhargav.boppana@gsa.gov",
+      "domain": "Entity Information",
+      "format": "Full CSV",
+      "fileName": "EntityInformation-20200504-085129_Entity",
+      "searchType": "Search Results",
+      "createDate": "May 4, 2020",
+      "expireDate": "May 11, 2020",
+      "status": "Expired"
+    },
+    {
+      "id": 749031,
+      "userId": "bhargav.boppana@gsa.gov",
+      "domain": "Contract Data",
+      "format": "Full CSV",
+      "fileName": "ContractData-20200429-022140",
+      "searchType": "Search Results",
+      "createDate": "Apr 29, 2020",
+      "expireDate": "May 6, 2020",
+      "status": "Expired"
+    },
+    {
+      "id": 747964,
+      "userId": "bhargav.boppana@gsa.gov",
+      "domain": "Entity Information",
+      "format": "Full CSV",
+      "fileName": "EntityInfor20200408-050809",
+      "searchType": "Search Results",
+      "createDate": "Apr 8, 2020",
+      "expireDate": "Apr 15, 2020",
+      "status": "Expired"
+    },
+    {
+      "id": 747962,
+      "userId": "bhargav.boppana@gsa.gov",
+      "domain": "Entity Information",
+      "format": "Full CSV",
+      "fileName": "EntityInformation-20200408-050809",
+      "searchType": "Search Results",
+      "createDate": "Apr 8, 2020",
+      "expireDate": "Apr 15, 2020",
+      "status": "Expired"
+    },
+    {
+      "id": 149491,
+      "userId": "matthew.finnell+3@gsa.gov",
+      "domain": "Entity Exclusion Information",
+      "format": "Full CSV",
+      "fileName": "EntityInformation-20200413-045704",
+      "searchType": "Search Results",
+      "createDate": "May 21, 2020",
+      "expireDate": "May 28, 2020",
+      "status": "Active"
+    },
+    {
+      "id": 747947,
+      "userId": "bhargav.boppana@gsa.gov",
+      "domain": "Entity Information",
+      "format": "Full CSV",
+      "fileName": "EntityInformation-20200408-044220",
+      "searchType": "Search Results",
+      "createDate": "Apr 8, 2020",
+      "expireDate": "Apr 15, 2020",
+      "status": "Expired"
+    },
+    {
+      "id": 149491,
+      "userId": "matthew.finnell+3@gsa.gov",
+      "domain": "Entity Exclusion Information",
+      "format": "Full CSV",
+      "fileName": "ExclusionInformation-20200411-045704",
+      "searchType": "Search Results",
+      "createDate": "May 21, 2020",
+      "expireDate": "May 28, 2020",
+      "status": "Active"
+    },
+    {
+      "id": 149499,
+      "userId": "matthew.finnell+3@gsa.gov",
+      "domain": "Entity Information",
+      "format": "Full CSV",
+      "fileName": "EntityInformation-045711",
+      "searchType": "Search Results",
+      "createDate": "May 22, 2020",
+      "expireDate": "May 29, 2020",
+      "status": "Active"
+    },
+    {
+      "id": 749533,
+      "userId": "bhargav.boppana@gsa.gov",
+      "domain": "CFDA",
+      "format": "CSV",
+      "fileName": "CFDA-140",
+      "searchType": "Search Results",
+      "createDate": "May 13, 2020",
+      "expireDate": "May 20, 2020",
+      "status": "Expired"
+    },
+    {
+      "id": 749669,
+      "userId": "bhargav.boppana@gsa.gov",
+      "domain": "CFDA",
+      "format": "PDF",
+      "fileName": "CFDA-PDF",
+      "searchType": "Search Results",
+      "createDate": "May 25, 2020",
+      "expireDate": "June 2, 2020",
+      "status": "Active"
+    },
+    {
+      "id": 742125,
+      "userId": "bhargav.boppana@gsa.gov",
+      "domain": "Entity Information",
+      "format": "Full CSV",
+      "fileName": "EntityInformation-20200504-085129_Entity",
+      "searchType": "Search Results",
+      "createDate": "May 25, 2020",
+      "expireDate": "June 2, 2020",
+      "status": "Active"
+    },
+    {
+      "id": 71,
+      "userId": "bhargav.boppana@gsa.gov",
+      "domain": "Federal Hierarchy",
+      "format": "CSV",
+      "fileName": "FH-140",
+      "searchType": "Search Results",
+      "createDate": "Apr 29, 2020",
+      "expireDate": "May 6, 2020",
+      "status": "Expired"
+    },
+    {
+      "id": 74,
+      "userId": "bhargav.boppana@gsa.gov",
+      "domain": "Federal Hierarchy",
+      "format": "PDF",
+      "fileName": "FH-050809",
+      "searchType": "Search Results",
+      "createDate": "Apr 8, 2020",
+      "expireDate": "Apr 15, 2020",
+      "status": "Expired"
+    },
+    {
+      "id": 72,
+      "userId": "bhargav.boppana@gsa.gov",
+      "domain": "Federal Hierarchy",
+      "format": "PDF",
+      "fileName": "FH-050809",
+      "searchType": "Search Results",
+      "createDate": "May 26, 2020",
+      "expireDate": "June 2, 2020",
+      "status": "Active"
+    },
+    {
+      "id": 191,
+      "userId": "matthew.finnell+3@gsa.gov",
+      "domain": "Federal Hierarchy",
+      "format": "PDF",
+      "fileName": "FH-050809",
+      "searchType": "Search Results",
+      "createDate": "May 26, 2020",
+      "expireDate": "June 2, 2020",
+      "status": "Active"
+    },
+    {
+      "id": 747,
+      "userId": "bhargav.boppana@gsa.gov",
+      "domain": "CFDA",
+      "format": "Full CSV",
+      "fileName": "CFDA220",
+      "searchType": "Search Results",
+      "createDate": "May 26, 2020",
+      "expireDate": "June 2, 2020",
+      "status": "Active"
+    },
+    {
+      "id": 149491,
+      "userId": "matthew.finnell+3@gsa.gov",
+      "domain": "Federal Hierarchy",
+      "format": "PDF",
+      "fileName": "FH_DataSvc_Extract_test1",
+      "searchType": "Search Results",
+      "createDate": "May 26, 2020",
+      "expireDate": "June 2, 2020",
+      "status": "Active"
+    },
+    {
+      "id": 199,
+      "userId": "matthew.finnell+3@gsa.gov",
+      "domain": "CFDA",
+      "format": "Full CSV",
+      "fileName": "CFDA045711",
+      "searchType": "Search Results",
+      "createDate": "May 21, 2020",
+      "expireDate": "May 28, 2020",
+      "status": "Active"
+    },
+  ]
+}
+];
