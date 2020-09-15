05c2155626545857f9684c48bc42f7b026b754cb
nikhithagobbura <nikhitha.gobbura@gsa.gov>
2020-09-15 14:26:48 -0400

    Child component for entity-display

---
+++ src/app/integrity-info/entity-display/entity-display.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-entity-display',
+  templateUrl: './entity-display.component.html',
+  styleUrls: ['./entity-display.component.css']
+})
+export class EntityDisplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
